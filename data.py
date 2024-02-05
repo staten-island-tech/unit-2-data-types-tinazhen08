e713d89a61adf253fd5a4b2e59d54285ed66c1e9
tinazhen08 <tinaz43@nycstudents.net>
2024-02-05 13:58:47 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,26 @@
-values = [1,2.23,5,7,2,30,15]
-print(values)
-for i in values:
-    print(i)
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+""" 
+my_favorite_color = input("what colors are in the rainbow?")
+if my_favorite_color  == "Blue":
+    print("correct")
+else:
+    print("incorrect") """
+
+""" x = "friend"
+print(f"hello {x}") """
+
+""" temp = 68
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+x = "0,2,4,6,8,"
+y = "1,3,5,7,9"
+
