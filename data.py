ced2bc5e2cd9888779fcabcd2258caa183bd3a25
tinazhen08 <tinaz43@nycstudents.net>
2024-02-07 13:34:28 -0500

    Update data.py

--- data.py
+++ data.py
@@ -29,19 +29,24 @@ else:
     print("odd")
  """
 
+print("what is your subtotal?")
 
 def subtotal():
     global subtotal
     subtotal = float(input("> $"))
 
 def tip():
-    print("what is your subtotal?")
-    
+    print("how was the service?")
+
     service = input(">")
     if ((service) == "bad"):
         print(f"do not tip")
     elif((service) == "okay"):
-        print(f"tip them {subtotal * float(0.15)}")
+        print(f"tip them ${subtotal * float(0.15)}")
+    elif((service) == "good"):
+        print(f"tip them ${subtotal * float(0.20)}")
+    elif((service) == "great"):
+        print(f"tip them ${subtotal * float(0.25)}")
 
 subtotal()
 tip()
