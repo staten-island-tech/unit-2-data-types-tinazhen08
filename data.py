a1591bb6af2d829e043761ae7e1e3c56fa03d552
tinazhen08 <tinaz43@nycstudents.net>
2024-02-07 13:40:47 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,53 +1,5 @@
-""" x = "this is a thing"
+x = "this is a thing"
 y= x.split( )
 z = y[0]
 print(y)
-print(z) """
-""" 
-my_favorite_color = input("what colors are in the rainbow?")
-if my_favorite_color  == "Blue":
-    print("correct")
-else:
-    print("incorrect") """
-10
-""" x = "friend"
-print(f"hello {x}") """
-
-""" temp = 68
-if temp > 68:
-    print('warm')
-elif temp == 68:
-    print('perfect')
-else:
-    print('cold') """
-
-""" #even or odd
-even_or_odd = int (input ("Even or Odd"))
-if ((even_or_odd) % 2) == 0:
-    print("even")
-else:
-    print("odd")
- """
-
-print("what is your subtotal?")
-
-def subtotal():
-    global subtotal
-    subtotal = float(input("> $"))
-
-def tip():
-    print("how was the service?")
-
-    service = input(">")
-    if ((service) == "bad"):
-        print(f"do not tip")
-    elif((service) == "okay"):
-        print(f"tip them ${subtotal * float(0.15)}")
-    elif((service) == "good"):
-        print(f"tip them ${subtotal * float(0.20)}")
-    elif((service) == "great"):
-        print(f"tip them ${subtotal * float(0.25)}")
-
-subtotal()
-tip()
-    
+print(z) 
