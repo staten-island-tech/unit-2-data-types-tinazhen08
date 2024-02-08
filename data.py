8a7f22ba2a4f9e4a6d7578986d4005495f559f87
tinazhen08 <tinaz43@nycstudents.net>
2024-02-08 13:59:46 -0500

    Update data.py

--- data.py
+++ data.py
@@ -3,3 +3,11 @@ y= x.split( )
 z = y[0]
 print(y)
 print(z) 
+
+def wordcount():
+    give_me_a_sentence = input("give me a sentence")
+    word = give_me_a_sentence.split()
+    print(word)
+    
+
+wordcount()
