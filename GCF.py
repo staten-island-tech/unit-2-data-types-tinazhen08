3a34b7d4e109221a48dea0ff8dc2c6fb913ce6dc
tinazhen08 <tinaz43@nycstudents.net>
2024-02-08 13:49:53 -0500

    Update GCF.py

--- GCF.py
+++ GCF.py
@@ -1,6 +1,10 @@
-num1 = int(input("give me a number"))
-num2 = int(input("give me another number"))
-
-def GCF():
+def GCF(x,y):
     global GCF
-    
+    if y == 0:
+        return x
+    else:
+        return GCF(y,x % y)
+x = int(input("give me a number"))
+y = int(input("give me another number"))
+
+print("The GCF is:", GCF(x,y))
