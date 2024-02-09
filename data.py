5a88b70bd92a0ad9e9a09447436608b802fe8547
tinazhen08 <tinaz43@nycstudents.net>
2024-02-09 13:36:43 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,13 +1,7 @@
-x = "this is a thing"
-y= x.split( )
-z = y[0]
-print(y)
-print(z) 
-
+#Challenge: Ask user to input sentence, accept sentence, and tell user how many words are in the sentence
 def wordcount():
     give_me_a_sentence = input("give me a sentence")
     word = give_me_a_sentence.split()
-    print(word)
+    print(len(word))
     
-
 wordcount()
