7b6bc49f0dd58bfde4a4febd08ab897516558e87
tinazhen08 <tinaz43@nycstudents.net>
2024-02-12 13:56:20 -0500

    Update GCF.py

--- GCF.py
+++ GCF.py
@@ -1,10 +1,14 @@
-def GCF(x,y):
-    global GCF
-    if y == 0:
-        return x
-    else:
-        return GCF(y,x % y)
 x = int(input("give me a number"))
 y = int(input("give me another number"))
+
+def GCF(x,y):
+    if x < y:
+        smaller_number = x
+    else:
+        smaller_number = y
+    for i in range(1,smaller_number + 1):
+        if((x%i == 0) and (y%i == 0)):
+            gcf = i
+    return gcf
 
 print("The GCF is:", GCF(x,y))
